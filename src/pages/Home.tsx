57dbcd85eb6565ce13fa1df71b48a554c0dfa4d5
gregorylira <gregory.lira007@gmail.com>
2021-11-15 15:58:52 -0300

    dia 3

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,12 +11,15 @@ import "../styles/auth.scss"
 
 import { Button } from '../components/Button';
 import { useAuth } from '../hooks/useAuth';
+import { FormEvent, useState } from 'react';
+import { database } from '../services/firebase';
 
 
 
 export function Home(){
     const history = useHistory();
     const {user, signInWithGoogle} = useAuth();
+    const [roomCode, setRoomCode] = useState("");
 
     async function handleCreateRoom(){
         if(!user){
@@ -25,6 +28,24 @@ export function Home(){
 
         history.push('/rooms/new');
 
+    }
+
+    async function handleJoinRoom(event:FormEvent) {
+        event.preventDefault();
+
+        if (roomCode.trim() === "") {
+            return;
+        }
+        
+
+        const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+        if(!roomRef.exists()){
+            alert("Sala não encontrada");
+            return;
+        }
+
+        history.push(`/rooms/${roomCode}`);
     }
 
     return (
@@ -42,10 +63,12 @@ export function Home(){
                         Crie sua sala com o Google
                     </button>
                     <div className="separator">ou entre em uma sala</div>
-                    <form>
+                    <form onSubmit={handleJoinRoom}>
                         <input 
                             type="text" 
                             placeholder="Digite o codigo da sala"
+                            onChange={(event) => setRoomCode(event.target.value)}
+                            value={roomCode}
                         />
                         <Button type="submit">
                             Entrar na sala
--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,6 +1,6 @@
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
+import 'firebase/compat/database';
 
 
 const firebaseConfig = {
@@ -17,6 +17,6 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 const auth = firebase.auth();
-const database = firebase.firestore();
+const database = firebase.database();
 
 export { firebase ,auth, database };
